fe8e829ebd625eb17a3db5bb93cf5e378785b0e6
Daniel Knowles <danoladesign@gmail.com>
2021-04-05 18:43:02 -0500

    map and api adjustments

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import Map from './Map.jsx';
+import Search from './Search.jsx'; 
 import Calendar from './Calendar.jsx';
 import UserProfile from './UserProfile.jsx';
-import { withScriptjs, withGoogleMap } from 'react-google-maps'; 
-import { apiKey } from '../../../key.js';
 
 const AppStyles = styled.div`
 `;
@@ -18,26 +17,19 @@ class App extends Component {
 
   render() {
    
-    const WrappedMap = withScriptjs(withGoogleMap(Map)); 
 
     return (
       <AppStyles>
         <div>
-        <header>
+          <header>
             <h1>Bike Around and Find Out</h1>
             <h3>New Orleans Interactive Community Bike Map</h3>
           </header>
-        <div style={{width: '100vw', height: '100vh'}}>
-          <WrappedMap
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${apiKey}`}
-            loadingElement={<div style={{ height: '100%' }} />}
-            containerElement={<div style={{ height: '100%' }} />}
-            mapElement={<div style={{ height: '100%' }} />}
-          />
+          <Map />
         </div>
         <Calendar />
         <UserProfile />
-      </div>
+        
       </AppStyles>
  
     );
